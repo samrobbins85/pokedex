61362a199ca7c4b5f1cc6837d68e3b4b5a6c71a7
Sam Robbins <samrobbinsgb@gmail.com>
2021-02-24 08:44:06 +0000

    Remove clear button as some browsers have it natively

--- components/search.js
+++ components/search.js
@@ -1,22 +1,10 @@
 import { useSearchFieldState } from "@react-stately/searchfield";
 import { useSearchField } from "@react-aria/searchfield";
 import { useRef } from "react";
-import { useButton } from "@react-aria/button";
-import Clear from "./svg/clear";
 export default function SearchField(props) {
     let state = useSearchFieldState(props);
     let ref = useRef();
-    let { inputProps, clearButtonProps } = useSearchField(props, state, ref);
-    let { buttonProps } = useButton(clearButtonProps);
+    let { inputProps } = useSearchField(props, state, ref);
 
-    return (
-        <div className="relative">
-            <input {...inputProps} ref={ref} />{" "}
-            {state.value !== "" && (
-                <button className="absolute right-2 top-2" {...buttonProps}>
-                    <Clear className="h-6 w-6 text-gray-700" />
-                </button>
-            )}
-        </div>
-    );
+    return <input {...inputProps} ref={ref} />;
 }
